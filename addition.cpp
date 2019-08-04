d18a45ecca472721b5e64a594f10f1e186235c44
PoojaB01 <45728538+PoojaB01@users.noreply.github.com>
2019-08-05 01:48:16 +0530

    Add files via upload

--- addition.cpp
+++ addition.cpp
@@ -11,7 +11,7 @@ vector<int> add(vector<int> a,vector<int> b)
 			v[i]+=a[i];
 		if(i<b.size())
 			v[i]+=b[i];
-			v[i]+=t;
+		v[i]+=t;
 		if(v[i]>9)
 		{
 			v[i]-=10;
@@ -19,74 +19,62 @@ vector<int> add(vector<int> a,vector<int> b)
 		}
 		else 
 			t=0;
-	//cout<<v[i]<<endl;
+		//cout<<v[i]<<endl;
 	}
+	for(int i=v.size()-1;i>=0;i--)
+		if(v[i]==0)
+			v.erase(v.begin()+i);
+		else break;
 	return v;
 }
 
 int main()
 {
-int q;
-cin>>q;
-getchar();
-while(q--)
-{
-vector<int> a,b;
-char c;
-int t;
-while(1)
-{
-	
-	
-	c=getchar();
-	if(c==' '||c=='\n')
-	{break;}
-	a.push_back(c-'0');
-}
-while(1)
-{
+	int q;
+	cin>>q;
+	getchar();
+	while(q--)
+	{
+		vector<int> a,b;
+		char c;
+		int t;
+		while(1)			//input a
+		{
+			c=getchar();
+			if(c==' '||c=='\n')
+			{break;}
+			a.push_back(c-'0');
+		}
+		while(1)			//input b
+		{
+			c=getchar();
+			if(c==' '||c=='\n')
+			{break;}
+			b.push_back(c-'0');
+		}
 
-	
-	c=getchar();
-	if(c==' '||c=='\n')
-	{break;}
-	b.push_back(c-'0');
-}
-/*for(int i=0;i<a.size();i++)
-cout<<a[i]<<' ';
-cout<<endl;
-for(int i=0;i<b.size();i++)
-cout<<b[i]<<' ';
-cout<<endl<<a.size()<<' '<<b.size();
-//cout<<'p';*/
-for(int i=0;i<a.size()/2;i++)
-{
-	t=a[i];
-	a[i]=a[a.size()-1-i];
-	a[a.size()-1-i]=t;
-}
-for(int i=0;i<b.size()/2;i++)
-{
-	t=b[i];
-	b[i]=b[b.size()-1-i];
-	b[b.size()-1-i]=t;
-}
-/*
-for(int i=0;i<a.size();i++)
-cout<<a[i]<<' ';
-cout<<endl;
-for(int i=0;i<b.size();i++)
-cout<<b[i]<<' ';
-cout<<endl;*/
-vector<int> v=add(a,b);
-		//cout<<1<<endl;
-for(int i=v.size()-1;i>=0;i--)
-{
-	if(i==v.size()-1&&v[i]==0)
-		continue;
-	cout<<v[i];
-} 
-	cout<<endl;
-}
-return 0;
+		for(int i=0;i<a.size()/2;i++)		//reverse a
+		{
+			t=a[i];
+			a[i]=a[a.size()-1-i];
+			a[a.size()-1-i]=t;
+		}
+		for(int i=0;i<b.size()/2;i++)		//reverse b
+		{
+			t=b[i];
+			b[i]=b[b.size()-1-i];
+			b[b.size()-1-i]=t;
+		}
+
+
+		vector<int> v=add(a,b);				//call function
+		for(int i=v.size()-1;i>=0;i--)		//display
+		{
+			cout<<v[i];
+		} 
+		if(v.size()==0)
+			cout<<0;
+		cout<<endl;
+	}
+	return 0;
 }
--- prime.cpp
+++ prime.cpp
@@ -9,96 +9,86 @@ int main()
 	getchar();
 	while(q--)
 	{
-	vector<int> a;
-	char c;
-	int t;
-	int y=0;
-	while(1)
-	{
-		c=getchar();
-		if(c==' '||c=='\n')
-		{break;}
-		a.push_back(c-'0');
-	}
-	if(a.size()==1&&a[0]==1)
+		vector<int> a;
+		char c;
+		int t;
+		int y=0;
+		while(1)
+		{
+			c=getchar();
+			if(c==' '||c=='\n')
+			{break;}
+			a.push_back(c-'0');
+		}
+		if(a.size()==1&&a[0]==1)
 		{
 			cout<<endl<<"Not a Prime";
 			continue;
 		}
-	int d;
-	vector<int> b(1,2),O(1,1);
-	for(;b.size()<=a.size()/2+1;)
-	{
-		vector<int> u(b.size()-1);
-		for(int i=0;i<b.size()-1;i++)
-		u[u.size()-1-i]=a[i];
-		vector<int> o(1,1);
-		for(int i=b.size()-1;i<a.size();i++)
+		int d;
+		vector<int> b(1,2),O(1,1);
+		for(;b.size()<=a.size()/2+1;)
 		{
-			d=0;
-			u.insert(u.begin(),a[i]);
-			for(int j=u.size()-1;j>=0;j--)
+			vector<int> u(b.size()-1);
+			for(int i=0;i<b.size()-1;i++)
+				u[u.size()-1-i]=a[i];
+			vector<int> o(1,1);
+			for(int i=b.size()-1;i<a.size();i++)
 			{
-				if(!u[j])
-					u.erase(u.begin()+j);
-				else break;
-			}
-			int f=1;
-			while(f)
-			{
-		
-				if(u.size()<b.size())
-					f=0;
-				else if(u.size()==b.size())
-				{
-					for(int j=u.size()-1;j>=0;j--)
-						if(u[j]<b[j])
-							{ f=0; break; }
-						else if(u[j]>b[j])
-						{
-							f=1; break;
-						}
-				}
-				if(!f)
-					break;
-				for(int j=0;j<u.size();j++)
-				{
-					if(j<b.size())
-						u[j]-=b[j];
-					if(u[j]<0)
-					{
-						u[j]+=10;
-						u[j+1]--;
-					}
-				}
+				d=0;
+				u.insert(u.begin(),a[i]);
 				for(int j=u.size()-1;j>=0;j--)
 				{
-					if(u[j]==0)
+					if(!u[j])
 						u.erase(u.begin()+j);
 					else break;
 				}
-				
-				/*t=0;
-				for(int j=0;j<max(d.size(),o.size());j++)
+				int f=1;
+				while(f)
 				{
-					if(j<o.size())
-						d[j]+=o[j];
-						d[j]+=t;
-					t=d[j]/10;
-					d[j]=d[j]%10;
-				}*/
-				if(d==0)
-				d=1;
-				else if(d==1)
-					d=2;
+
+					if(u.size()<b.size())
+						f=0;
+					else if(u.size()==b.size())
+					{
+						for(int j=u.size()-1;j>=0;j--)
+							if(u[j]<b[j])
+							{ f=0; break; }
+							else if(u[j]>b[j])
+							{
+								f=1; break;
+							}
+					}
+					if(!f)
+						break;
+					for(int j=0;j<u.size();j++)
+					{
+						if(j<b.size())
+							u[j]-=b[j];
+						if(u[j]<0)
+						{
+							u[j]+=10;
+							u[j+1]--;
+						}
+					}
+					for(int j=u.size()-1;j>=0;j--)
+					{
+						if(u[j]==0)
+							u.erase(u.begin()+j);
+						else break;
+					}
+					if(d==0)
+						d=1;
+					else if(d==1)
+						d=2;
+				}
 			}
-		}
-		if(u.size()==0&&(d!=1))
-		{
-			cout<<endl<<"Not a Prime";
-			y=1;
-			break;
-		}
+			if(u.size()==0&&(d!=1))
+			{
+				cout<<"Not a Prime"<<endl;
+				y=1;
+				break;
+			}
 			t=0;
 			for(int j=0;j<max(b.size(),o.size());j++)
 			{
@@ -107,15 +97,15 @@ int main()
 				b[j]+=t;
 				t=b[j]/10;
 				b[j]=b[j]%10;
-			//cout<<v[i]<<endl;
+				//cout<<v[i]<<endl;
 			}
 			if(t)
 				b.push_back(t);
-		
+
+		}
+		if(y==0)
+			cout<<"Prime"<<endl;
 	}
-	if(y==0)
-	cout<<endl<<"Prime";
-}
-cout<<endl;
+	cout<<endl;
 	return 0;
 }
