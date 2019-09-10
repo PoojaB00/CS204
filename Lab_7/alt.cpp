14702fd7381d7b547f629525a4c21cc4708fb1fd
PoojaB01 <45728538+PoojaB01@users.noreply.github.com>
2019-09-11 02:33:49 +0530

    Lab_7 completed

--- Lab_7/alt.cpp
+++ Lab_7/alt.cpp
@@ -10,23 +10,12 @@ vector<vector<pair<string,int>>> radixsort(vector<vector<pair<string,int>>> v,in
 	{
 		for(int i=0;i<v[k].size();i++)
 		{
-			// if(v[k][i].length()==n-1)
-			// 	v1[(v[k][i][0]-'0')*2+2].push_back(v[k][i]);
-			// else 
-			v1[v[k][i].first[n-1]-'0'].push_back(v[k][i]);
-			//cout<<v[k][i].first<<' ';
+			v1[v[k][i].first[(n-1)%v[k][i].second]-'0'].push_back(v[k][i]);
 		}
 	}
-	//cout<<1;
 	if(n==1)
 		return v1;
-	// for(int k=0;k<10;k++)
-	// 	{
-	// 		for(int i=0;i<v1[k].size();i++)
-	// 			cout<<v1[k][i].first<<' ';
-	// 		cout<<endl;
-	// 	}
-	 return radixsort(v1,n-1);
+	return radixsort(v1,n-1);
 }
 
 int main()
@@ -39,21 +28,11 @@ int main()
 		string s;
 		cin>>q;
 		vector<vector<pair<string,int>>> v(10),v1;
-		string s1="0000000000000000000000000000000000000000";
 		for(int i=0;i<q;i++)
 		{
 			cin>>s;
-			for(int i=0;i<41;i++)
-			s1[i]=s[i%s.length()];
-			v[s[0]-'0'].push_back({s1,s.length()});
+			v[s[0]-'0'].push_back({s,s.length()});
 		}
-		//cout<<1;
-		// for(int k=0;k<10;k++)
-		// {
-		// 	for(int i=0;i<v[k].size();i++)
-		// 		cout<<v[k][i].first<<' '<<v[k][i].second;
-		// 	cout<<endl;
-		// }
 		v1=radixsort(v,40);
 		for(int i=9;i>=0;i--)
 		{
@@ -61,13 +40,8 @@ int main()
 				for(int j=0;j<v1[i][k].second;j++)
 					cout<<v1[i][k].first[j];
 		}
-		// for(int k=0;k<10;k++)
-		// {
-		// 	for(int i=0;i<v1[k].size();i++)
-		// 		cout<<v1[k][i].first<<' '<<v1[k][i].second;
-		// 	cout<<endl;
-		// }
 		cout<<endl;
 	}
 	return 0;
 }
+
